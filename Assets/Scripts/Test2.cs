1900f81f308709aed33f04f10b5b9dd1a74911e4
VladSith <swkotorsotton@gmail.com>
2022-07-04 17:25:19 +0300

    2
    Notifiacation manager script

--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -7,7 +7,8 @@ public class Test2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var cls = new ClassLibrary1.Class1();
+        Debug.Log("test = " + cls.RandomValue());
     }
 
     // Update is called once per frame
