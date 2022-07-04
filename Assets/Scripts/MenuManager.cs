07d75e2cd9819ef49796195ae714094704b44f9e
VladSith <swkotorsotton@gmail.com>
2022-07-04 15:11:06 +0300

    UI
    UI

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject pauseMenuCanvas;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+        
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+        AudioListener.pause = false;
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Pause1()
+    {
+
+        pauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f;
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+}
